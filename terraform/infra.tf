093b14cd2adaaf23ec4d508cfbfd6afd550f8c03
louis <louisalexhebert@gmail.com>
2022-12-21 16:23:10 -0500

    add config to standalone instance

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -7,6 +7,9 @@ resource "aws_instance" "standalone" {
   ami           = "ami-0574da719dca65348"
   instance_type = "t2.micro"
   associate_public_ip_address = true
+  user_data = templatefile("../scripts/standalone-config.sh.tftpl", {
+    number = count.index
+  })
   subnet_id              = aws_subnet.subnet1.id
   key_name = "FinalProject"
   vpc_security_group_ids = [aws_security_group.ssh.id]
