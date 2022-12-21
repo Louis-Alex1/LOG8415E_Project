4429a47ca256afae54554542bb8a223fe51e6b7a
louis <louisalexhebert@gmail.com>
2022-12-21 15:03:13 -0500

    add VPC config

--- terraform/network.tf
+++ terraform/network.tf
@@ -2,26 +2,59 @@
 # network.tf
 # Terraform configuration relative to network definitions
 
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
 }
 
 resource "aws_subnet" "subnet1" {
-  vpc_id            = aws_vpc.my_vpc.id
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "subnet2" {
-  vpc_id            = aws_vpc.my_vpc.id
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1a"
 }
 
+# Virtual private cloud configuration
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "rt_a_standalone" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "rt_a_cluster" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_security_group" "ssh" {
   name  = "SSH"
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.vpc.id
 
   ingress {
     from_port        = 22
