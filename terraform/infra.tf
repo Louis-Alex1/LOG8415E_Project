dbf818b1ce51884bed122a9e49a1c84b8809bc0f
louis <louisalexhebert@gmail.com>
2022-12-21 13:41:40 -0500

    add private ip

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -7,7 +7,11 @@ resource "aws_instance" "standalone" {
   ami           = "ami-0574da719dca65348"
   instance_type = "t2.micro"
   associate_public_ip_address = true
-  subnet_id              = aws_subnet.network1.id
+  subnet_id              = aws_subnet.subnet1.id
+  network_interface {
+    network_interface_id = "${aws_network_interface.interface1.id}"
+    device_index = 0
+  }
 }
 
 resource "aws_instance" "cluster" {
@@ -15,5 +19,9 @@ resource "aws_instance" "cluster" {
   ami           = "ami-0574da719dca65348"
   instance_type = "t2.micro"
   associate_public_ip_address = true
-  subnet_id              = aws_subnet.network2.id
+  subnet_id              = aws_subnet.subnet2.id
+  network_interface {
+    network_interface_id = "${aws_network_interface.interface2.id}"
+    device_index = 0
+  }
 }
--- terraform/network.tf
+++ terraform/network.tf
@@ -6,14 +6,24 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "network1" {
+resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "network2" {
+resource "aws_network_interface" "interface1" {
+  subnet_id   = aws_subnet.subnet1.id
+  private_ips = ["10.0.1.1"]
+}
+
+resource "aws_subnet" "subnet2" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1a"
+}
+
+resource "aws_network_interface" "interface2" {
+  subnet_id   = aws_subnet.subnet2.id
+  private_ips = ["10.0.2.1", "10.0.2.2", "10.0.2.3", "10.0.2.4"]
 }
