0f48624f575838d30496e2ee28fc2e365147de9f
louis <louisalexhebert@gmail.com>
2022-12-23 11:42:24 -0500

    move proxy inside cluster for same private network

--- cluster/terraform/infra.tf
+++ cluster/terraform/infra.tf
@@ -15,3 +15,18 @@ resource "aws_instance" "cluster" {
     Name = "cluster"
   }
 }
+
+# Start one t2.large instance for the proxy
+resource "aws_instance" "proxy" {
+  count         = 1
+  ami           = "ami-0a6b2839d44d781b2"
+  instance_type = "t2.large"
+  associate_public_ip_address = true
+  user_data = templatefile("../scripts/proxy-config.sh.tftpl", {})
+  subnet_id              = aws_subnet.subnet1.id
+  key_name = "FinalProject"
+  vpc_security_group_ids = [aws_security_group.proxy_security_group.id]
+  tags = {
+    Name = "proxy"
+  }
+}
--- cluster/terraform/network.tf
+++ cluster/terraform/network.tf
@@ -44,7 +44,7 @@ resource "aws_route_table_association" "rt_a_cluster" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# Security groups to allow the use of different ports
+# Cluster security group to allow the use of different ports
 resource "aws_security_group" "cluster_security_group" {
   name  = "Cluster security group"
   vpc_id = aws_vpc.vpc.id
@@ -81,6 +81,40 @@ resource "aws_security_group" "cluster_security_group" {
     from_port        = 30000
     to_port          = 65535
     protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+# Proxy security groups to allow the use of different ports
+resource "aws_security_group" "proxy_security_group" {
+  name  = "Proxy security group"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 5000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 8
+    to_port          = 0
+    protocol         = "icmp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
